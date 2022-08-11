e5b5c1832bb3af0e673186381b5534c20783ffdb
Paulo Romero <player_one_117@outlook.com>
2022-08-11 16:14:35 -0500

    Creación de tablas para la bd

--- database/migrations/2022_08_11_181539_create_courses_table.php
+++ database/migrations/2022_08_11_181539_create_courses_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateCoursesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,10 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('courses', function (Blueprint $table) {
             $table->id();
-            $table->string('img');
             $table->string('name');
-            $table->string('apellidoP');
-            $table->string('apellidoM');
-            $table->string('email')->unique();
-            $table->boolean('status');
+            $table->string('description');
         });
     }
 
@@ -31,6 +27,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('courses');
     }
 }
