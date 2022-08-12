1cb74e3b8230fcfcc3099c08b1e9a55c8b77b822
Paulo Romero <player_one_117@outlook.com>
2022-08-12 13:40:21 -0500

    Tabla transladada al componente course-user y correcta implementacion pasando parametros

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,46 +16,7 @@
 <body>
     <div class="container text-center">
         <h1 class="mb-5 fst-italic">Bienvenido</h1>
-
-        <table class="table">
-            <thead>
-                <!-- Primera fila de la tabla que contendra el encabezado -->
-                <tr scope="row" class="text-center fst-italic">
-                    <th scope="col">Image</th>
-                    <th scope="col">Nombre</th>
-                    <th scope="col">Apellido Paterno</th>
-                    <th scope="col">Apellido Materno</th>
-                    <th scope="col">Correo</th>
-                    <th scope="col">Cursos</th>
-                    <th scope="col">Estatus</th>
-                </tr>
-            </thead>
-            <tbody>
-                <!-- Las siguientes filas son creadas mediante bucles para llenar las columnas -->
-                @foreach ($users as $user)
-                <tr scope="row" class="text-center align-middle fst-italic">
-                    <td scope="col">{{$user->img}}</td>
-                    <td scope="col">{{$user->name}}</td>
-                    <td scope="col">{{$user->apellidoP}}</td>
-                    <td scope="col">{{$user->apellidoM}}</td>
-                    <td scope="col">{{$user->email}}</td>
-                    <td scope="col" class="text-start">
-                        <ul>
-                            @if ($user->courses->count() > 0)
-                            @foreach ($user->courses as $course)
-                            <li>{{$course->name}}</li>
-                            @endforeach
-                            @else
-                            <li>No hay cursos</li>
-                            @endif
-                        </ul>
-                    </td>
-                    <td scope="col" @class(['text-success'=> $user->status, 'text-danger' =>
-                        !$user->status])>{{$user->status ? 'Activo' : 'Inactivo'}}</td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
+        <x-tables.course-user :users="$users" />
     </div>
 </body>
 
